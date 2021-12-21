e9d05642ba7e6568e393dffa0446b41b6ff62b06
GalBenZvi <hershkovitz1@mail.tau.ac.il>
2021-12-21 12:29:44 +0200

    added bids filter to fmriprep interface

---
+++ django_mri/analysis/pipelines/the_base.py
@@ -0,0 +1,6 @@
+"""
+Simplified preprocessing pipeline for TheBase scanning protocol.
+"""
+FREESURFER_NODE = {"analysis_version": "ReconAll"}
+FMRIPREP_NODE = {"analysis_version": "fMRIPrep"}
+DMRIPREP_NODE = {"analysis_version": "dMRIPrep"}
